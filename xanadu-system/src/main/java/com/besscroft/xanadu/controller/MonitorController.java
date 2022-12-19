3bbc44a802896839cdcd0be9e9f6924020fabecc
besscroft <besscroft@foxmail.com>
2022-12-19 15:17:05 +0800

    feat:add system monitor

---
+++ xanadu-system/src/main/java/com/besscroft/xanadu/controller/MonitorController.java
@@ -0,0 +1,25 @@
+package com.besscroft.xanadu.controller;
+
+import com.besscroft.xanadu.common.dto.ServerInfo;
+import com.besscroft.xanadu.common.result.CommonResult;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description 系统监控
+ * @Author Bess Croft
+ * @Date 2022/12/19 14:25
+ */
+@RestController
+@RequestMapping("/monitor")
+public class MonitorController {
+
+    @Operation(summary = "系统信息接口")
+    @GetMapping("/serverInfo")
+    public CommonResult<ServerInfo> serverInfo() {
+        return CommonResult.success(ServerInfo.serverInfo());
+    }
+
+}
