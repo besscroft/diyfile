13104a9c37e9673b572f637113b948031760344c
besscroft <besscroft@foxmail.com>
2023-01-07 17:47:04 +0800

    doc:update xanadu.sql

--- doc/sql/xanadu.sql
+++ doc/sql/xanadu.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : k8s-dev
- Source Server Type    : MariaDB
- Source Server Version : 101002
+ Source Server         : xanadu
+ Source Server Type    : MySQL
+ Source Server Version : 101002 (10.10.2-MariaDB-1:10.10.2+maria~ubu2204)
  Source Host           : 140.238.17.232:30003
  Source Schema         : xanadu
 
- Target Server Type    : MariaDB
- Target Server Version : 101002
+ Target Server Type    : MySQL
+ Target Server Version : 101002 (10.10.2-MariaDB-1:10.10.2+maria~ubu2204)
  File Encoding         : 65001
 
- Date: 18/12/2022 21:18:59
+ Date: 07/01/2023 17:44:28
 */
 
 SET NAMES utf8mb4;
@@ -22,30 +22,31 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `storage`;
 CREATE TABLE `storage`  (
-  `id` bigint(20) NOT NULL,
+  `id` bigint NOT NULL,
   `name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '存储名称',
-  `type` tinyint(4) NULL DEFAULT NULL COMMENT '存储类型',
-  `enable` tinyint(4) NULL DEFAULT NULL COMMENT '存储启用状态：0->禁用；1->启用',
+  `type` tinyint NULL DEFAULT NULL COMMENT '存储类型：0->本地存储；1->OneDrive',
+  `enable` tinyint NULL DEFAULT 0 COMMENT '存储启用状态：0->禁用；1->启用',
   `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '备注',
-  `creator` bigint(20) NULL DEFAULT NULL COMMENT '创建者',
-  `updater` bigint(20) NULL DEFAULT NULL COMMENT '更新者',
+  `creator` bigint NULL DEFAULT NULL COMMENT '创建者',
+  `updater` bigint NULL DEFAULT NULL COMMENT '更新者',
   `create_time` datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
-  `del` tinyint(4) NOT NULL DEFAULT 1 COMMENT '逻辑删除：0->删除状态；1->可用状态',
+  `del` tinyint NOT NULL DEFAULT 1 COMMENT '逻辑删除：0->删除状态；1->可用状态',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '存储表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of storage
 -- ----------------------------
+INSERT INTO `storage` VALUES (1, '测试本地存储', 0, 1, '测试本地存储用的', 1, 1, '2022-12-29 23:07:48', '2022-12-29 23:08:00', 1);
 
 -- ----------------------------
 -- Table structure for storage_config
 -- ----------------------------
 DROP TABLE IF EXISTS `storage_config`;
 CREATE TABLE `storage_config`  (
-  `id` bigint(20) NOT NULL,
-  `storage_id` bigint(20) NOT NULL COMMENT '存储id',
+  `id` bigint NOT NULL,
+  `storage_id` bigint NOT NULL COMMENT '存储id',
   `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '存储配置名称',
   `key` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '存储配置键',
   `value` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '存储配置值',
@@ -56,13 +57,14 @@ CREATE TABLE `storage_config`  (
 -- ----------------------------
 -- Records of storage_config
 -- ----------------------------
+INSERT INTO `storage_config` VALUES (1, 1, '根路径', 'path', 'D:\\app-log', '访问本地存储的根路径');
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `username` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '账号(用户名)',
   `password` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '密码',
   `avatar` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '头像(地址)',
@@ -72,21 +74,27 @@ CREATE TABLE `user`  (
   `real_name` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '真实姓名',
   `telephone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '手机',
   `birthday` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '生日',
-  `sex` tinyint(4) NULL DEFAULT NULL COMMENT '性别：1->男；2->女；3->未知',
+  `sex` tinyint NULL DEFAULT NULL COMMENT '性别：1->男；2->女；3->未知',
   `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '备注',
-  `creator` bigint(20) NULL DEFAULT NULL COMMENT '创建者',
-  `updater` bigint(20) NULL DEFAULT NULL COMMENT '更新者',
+  `creator` bigint NULL DEFAULT NULL COMMENT '创建者',
+  `updater` bigint NULL DEFAULT NULL COMMENT '更新者',
   `create_time` datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
   `login_time` datetime NULL DEFAULT NULL COMMENT '最后登录时间',
-  `status` tinyint(4) NOT NULL DEFAULT 0 COMMENT '帐号启用状态：0->禁用；1->启用',
-  `del` tinyint(4) NOT NULL DEFAULT 1 COMMENT '逻辑删除：0->删除状态；1->可用状态',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+  `status` tinyint NOT NULL DEFAULT 0 COMMENT '帐号启用状态：0->禁用；1->启用',
+  `del` tinyint NOT NULL DEFAULT 1 COMMENT '逻辑删除：0->删除状态；1->可用状态',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `username`(`username` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'xanadu', '94edf28c6d6da38fd35d7ad53e485307f89fbeaf120485c8d17a43f323deee71', 'https://besscroft.com/uploads/avatar.jpeg', 'platform-admin', NULL, '旅行者', NULL, NULL, NULL, 1, NULL, 1, 1, '2022-12-18 17:08:17', NULL, '2022-12-18 17:07:07', 1, 1);
+INSERT INTO `user` VALUES (1, 'xanadu', '94edf28c6d6da38fd35d7ad53e485307f89fbeaf120485c8d17a43f323deee71', 'https://besscroft.com/uploads/avatar.jpeg', 'platform-super-admin', NULL, '旅行者', NULL, NULL, NULL, 1, '只要不失去你的崇高，整个世界都会向你敞开。', 1, 1, '2023-01-07 16:38:38', NULL, '2022-12-18 17:07:07', 1, 1);
+INSERT INTO `user` VALUES (2, 'test1', 'eqw', 'https://besscroft.com/uploads/avatar.jpeg', 'platform-admin', NULL, '测试', NULL, NULL, NULL, 1, '我超，原！只要不失去你的崇高，整个世界都会向你敞开。只要不失去你的崇高，整个世界都会向你敞开。只要不失去你的崇高，整个世界都会向你敞开。', 1, 1, '2023-01-07 16:42:08', NULL, '2022-12-19 17:14:39', 1, 1);
+INSERT INTO `user` VALUES (3, 'test2', 'eqw', 'https://besscroft.com/uploads/avatar.jpeg', 'platform-self-provisioner', NULL, '测试', NULL, NULL, NULL, 1, '我超，原！', 1, 1, '2023-01-07 16:42:10', NULL, '2022-12-19 17:14:39', 1, 1);
+INSERT INTO `user` VALUES (4, 'test3', 'eqw', 'https://besscroft.com/uploads/avatar.jpeg', 'platform-view', NULL, '测试', NULL, NULL, NULL, 1, '我超，原！', 1, 1, '2023-01-07 16:38:52', NULL, '2022-12-19 17:14:39', 1, 1);
+INSERT INTO `user` VALUES (5, 'test4', 'eqw', 'https://besscroft.com/uploads/avatar.jpeg', 'platform-visitor', NULL, '测试', NULL, NULL, NULL, 1, '我超，原！', 1, 1, '2023-01-07 16:42:17', NULL, '2022-12-19 17:14:39', 1, 1);
+INSERT INTO `user` VALUES (6, 'test5', 'eqw', 'https://besscroft.com/uploads/avatar.jpeg', 'platform-visitor', NULL, '测试', NULL, NULL, NULL, 1, '我超，原！', 1, 1, '2023-01-07 16:42:18', NULL, '2022-12-19 17:14:39', 1, 1);
 
 SET FOREIGN_KEY_CHECKS = 1;
